d93364398b572646c303a898ea84f7c205b53bf7
Kampanart Mekaumput <kampanartm.careers@gmail.com>
2025-02-21 07:51:20 +0700

    added function arn

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "function_name" {
   value =  module.lambda.function_name
 }
 
+output "function_arn" {
+  description = "ARN of the Lambda function."
+  value = module.lambda.function_invoke_arn
+}
+
 output "s3_bucket_id" {
   description = "ID of the S3 bucket storing the files."
   value = module.s3.s3_data_bucket_id
